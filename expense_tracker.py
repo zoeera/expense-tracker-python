b66cd08c1b897ad7abf3e5e860f35e1ea1cb8a71
Zoheba Kashaf <zohebakashaf@gmail.com>
2026-07-10 21:03:50 +0530

    Add file saving and loading using JSON

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,4 +1,14 @@
-expenses = []
+import json
+import os
+def save_expenses(expenses):
+	with open("expenses.json", "w") as f:
+		json.dump(expenses, f)
+def load_expenses():
+	if os.path.exists("expenses.json"):
+		with open("expenses.json", "r") as f:
+			return json.load(f)
+	return[]
+expenses = load_expenses()
 while True:
 	print("\n1. Add expense")
 	print("2. View all expenses")
@@ -11,6 +21,7 @@ while True:
 		description = input("Description: ")
 		expenses.append({"category": category, "amount": amount, "description": description})
 		print("Expense added!")
+		save_expenses(expenses)
 	elif choice == "2":
 		if len(expenses) == 0:
 			print("No expenses yet.")
@@ -26,3 +37,4 @@ while True:
 		break
 	else:
 		print("Invalid option. PLease choose 1-4.")
+```
